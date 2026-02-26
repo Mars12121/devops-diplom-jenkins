ede145cf11382b7abc82cae9ecdc8766e47372f5
Морозов Александр <sanchez12121@mail.ru>
2026-02-26 22:22:42 +0300

    tezgahsjm

--- deploy.groovy
+++ deploy.groovy
@@ -68,14 +68,16 @@ pipeline {
         stage('Docker Build & Push') {
             steps {
                 script {
-                    def fullImageName = "${REGISTRY}/${IMAGE_NAME}:${env.NEW_VERSION}"
-                    
-                withCredentials([
-                    string(credentialsId: YANDEX_CREDS, variable: 'docker_token')                    
-                ]) {
-                        sh "echo -n $docker_token | docker login --username oauth --password-stdin cr.yandex"
-                        sh "docker build -t ${fullImageName} ."
-                        sh "docker push ${fullImageName}"
+                    dir("devops-diplom-app") {
+                        def fullImageName = "${REGISTRY}/${IMAGE_NAME}:${env.NEW_VERSION}"
+                        
+                        withCredentials([
+                            string(credentialsId: YANDEX_CREDS, variable: 'docker_token')                    
+                        ]) {
+                                sh "echo -n $docker_token | docker login --username oauth --password-stdin cr.yandex"
+                                sh "docker build -t ${fullImageName} ."
+                                sh "docker push ${fullImageName}"
+                            }
                     }
                 }
             }
@@ -83,15 +85,17 @@ pipeline {
 
         stage('Git Push') {
             steps {
-                sshagent([env.SSH_CREDS_ID]) {
-                    sh """
-                            git config user.name "Морозов Александр"
-                            git config user.email "sanchez12121@mail.ru"
-                            
-                            git add .
-                            git commit -m "Version up ${env.NEW_VERSION} [ci skip]"
-                            git push origin main
-                        """
+                dir("devops-diplom-app") {
+                    sshagent([env.SSH_CREDS_ID]) {
+                        sh """
+                                git config user.name "Морозов Александр"
+                                git config user.email "sanchez12121@mail.ru"
+                                
+                                git add .
+                                git commit -m "Version up ${env.NEW_VERSION}"
+                                git push origin main
+                            """
+                    }
                 }
             }
         }
